75a73cd37f6d1b8138280dc5e40d287ee0a9e392
liuxiaojun <lxj_web@163.com>
2022-11-13 12:17:55 +0800

    feat: 登录

--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import useUserStore from '@/stores/modules/user'
+const userStore = useUserStore()
+const login = () => {
+  userStore.setToken('123456')
+  location.reload()
+}
+</script>
 
 <template>
-  <main>
-    <span>登录页面</span>
-  </main>
+  <ElContainer>
+    <ElHeader>
+      <ElRow>
+        <ElCol :span="12">
+          <ElInput placeholder="账号" />
+          <ElInput placeholder="密码" type="password" />
+          <ElButton @click="login" type="primary">登录</ElButton>
+        </ElCol>
+      </ElRow>
+    </ElHeader>
+  </ElContainer>
 </template>
