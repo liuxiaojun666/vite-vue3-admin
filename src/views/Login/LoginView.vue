9b03f71b45521113c4b0f08881cd39a01d64c65b
liuxiaojun <lxj_web@163.com>
2022-11-15 20:21:14 +0800

    fix(login): 跳转bug

--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import useUserStore from '@/stores/modules/user'
-import { useRouter } from 'vue-router'
 const userStore = useUserStore()
 const login = () => {
   userStore.setToken('123456')
-  useRouter().push('/')
+  location.reload()
 }
 </script>
 
